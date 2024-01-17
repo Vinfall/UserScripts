cbdf0a24a2f8df9b661858d25ae091563f3a157d
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-01-17 09:45:31 +0800

    Auto enter restricted page

--- Tiangal Old Driver.user.js
+++ Tiangal Old Driver.user.js
@@ -3,12 +3,20 @@
 // @namespace   https://github.com/Vinfall/UserScripts
 // @match       https://www.tiangal.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Vinfall
 // @description 天遊二次元默认开启老司机模式
 // ==/UserScript==
 (function () {
     'use strict';
+
+    window.addEventListener('load', function () {
+        var confirmAdultButton = document.querySelector('.swal2-styled.swal2-confirm');
+        if (confirmAdultButton) {
+            confirmAdultButton.click();
+        }
+    });
+
     var hiddenElement = document.querySelector('#hidedown');
     if (hiddenElement) {
         hiddenElement.style.display = 'block';
