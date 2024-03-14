3fd81e8d84c9a5b18c645d79098deafa218303bf
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-03-14 12:22:56 +0800

    steam: warn on unexpected feature

--- steam-game-feature-kai.user.js
+++ steam-game-feature-kai.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Game Feature Kai
 // @namespace    https://github.com/Vinfall/UserScripts
-// @version      2.0.0
+// @version      2.4.1
 // @author       WK, Vinfall
 // @match        https://store.steampowered.com/app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
@@ -19,7 +19,10 @@
         var appId = match[1];
         console.log(appId);
 
-        var conditions = [{
+        const warningColor = 'red';
+        var conditions = [
+            // Outlink replacement
+            {
                 srcIncludes: 'ico_cloud',
                 text: 'Cloudsave',
                 href: 'https://store.steampowered.com/account/remotestorageapp?appid=' + appId + '&index=0'
@@ -28,15 +31,33 @@
                 srcIncludes: 'ico_cards',
                 text: 'Steam Card Exchange',
                 href: 'https://www.steamcardexchange.net/index.php?gamepage-appid-' + appId + '/'
+            },
+            // Warning on unexpected feature
+            {
+                // 'ico_cart' is IAP
+                srcIncludes: ['ico_coop', 'ico_multiPlayer', 'ico_cart', 'ico_vac'],
+                color: warningColor
             }
         ];
 
+        // TODO: expand to div.page_content > .game_meta_data.rightcol for better matching
         var elements = document.querySelectorAll('a.game_area_details_specs_ctn');
         elements.forEach(function (element) {
             conditions.forEach(function (condition) {
-                if (element.querySelector('.icon img.category_icon').src.includes(condition.srcIncludes)) {
-                    element.href = condition.href;
-                    element.querySelector('.label').innerText = condition.text;
+                if (Array.isArray(condition.srcIncludes)) {
+                    if (condition.srcIncludes.some(src => element.querySelector('.icon img.category_icon').src.includes(src))) {
+                        // Set color on unwanted feature
+                        if (condition.color) {
+                            element.querySelector('.label').style.color = condition.color;
+                            element.querySelector('.label').style.fontWeight = 'bold';
+                        }
+                    }
+                } else {
+                    // Replace outlink otherwise
+                    if (element.querySelector('.icon img.category_icon').src.includes(condition.srcIncludes)) {
+                        element.href = condition.href;
+                        element.querySelector('.label').innerText = condition.text;
+                    }
                 }
             });
         });
