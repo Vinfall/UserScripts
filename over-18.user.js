99c7db328eb38cb447efbc6d3ddbd06407afa80e
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-09-08 19:51:49 +0800

    feat/18: add booth & toranoana

--- over-18.user.js
+++ over-18.user.js
@@ -2,10 +2,12 @@
 // @name              Over-18
 // @name:zh-cn        已满 18 岁
 // @namespace         https://github.com/Vinfall/UserScripts
-// @version           0.10.0
+// @version           0.12.1
 // @author            Vinfall
 // @match             https://*.itch.io/*
 // @match             https://*.reddit.com/over18?dest=*
+// @match             https://booth.pm/*/items/*
+// @match             https://ec.toranoana.jp/tora_r/ec/item/*
 // @match             https://gamebanana.com/mods/*
 // @match             https://gamejolt.com/games/*/*
 // @match             https://gamejolt.com/games/*/*/followers
@@ -24,7 +26,7 @@
 // @run-at            document-start
 // @license           CC0 1.0 Universal (Public Domain)
 // @icon              data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔞</text></svg>
-// @description       Certainly Not Minor, aka. CNM
+// @description       Certainly Not Minor, aka. CNM.R18
 // @description:zh-cn 因为我刚满 18 岁~
 // ==/UserScript==
 
@@ -34,6 +36,7 @@
     // Define rules
     const config = {
         'animategames.jp': '.btn-blr18.btn',
+        'booth.pm': '.js-approve-adult > .adult-check-nav',
         'digiket.com': '.btn-lg.btn-info.btn',
         'gamebanana.com': '.ShowNsfwContentButton',
         'gamejolt.com': '.link-muted > span', // this mutes until I exit
@@ -44,6 +47,7 @@
         'nintendo.com.hk': 'button#eco-product-confirmation-hide.action-primary',
         'patreon.com': 'button[data-tag="age-confirmation-button"]',
         'reddit.com': 'button.c-btn-primary.c-btn:nth-of-type(2)',
+        'toranoana.jp': '#js-en-confirm-adult > .c-modal_content_body > .text-center.c-modal_content_inner > .mt-4.ui-confirm-adult-btn-wrapper > .ui-confirm-adult-btn-primary.c-btn-black.c-btn.js-ui-modal-close.js-btn-primary',
     };
 
     function getSelectorForCurrentSite() {
