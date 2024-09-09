6fdf44b62beba0cdeb146c30506ad11bfcf1ea69
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-09-09 10:17:14 +0800

    chore: strict match

--- show-orig-pic.user.js
+++ show-orig-pic.user.js
@@ -2,7 +2,7 @@
 // @name              Show Original Picture
 // @name:zh-cn        自动跳转原图
 // @namespace         https://github.com/Vinfall/UserScripts
-// @version           0.3.1
+// @version           0.3.2
 // @author            Vinfall
 // @match             https://i*.hdslb.com/bfs/*/*.avif
 // @match             https://i*.hdslb.com/bfs/*/*.webp
@@ -24,7 +24,7 @@
     const urlReplacements = {
         // e.g. https://i0.hdslb.com/bfs/archive/bfa1134b7d3ab7fcbc363fd7f91be783fa64696c.jpg@320w_200h_1c_!web-space-index-myseries.avif
         'hdslb.com': (url) => url.replace(/(\.(jpg|jpeg|png|webp)).*?\.(avif|webp)$/, '$1'),
-        'gcores.com': (url) => url.split('?')[0],
+        'image.gcores.com': (url) => url.split('?')[0],
     };
 
     // Match pattern
