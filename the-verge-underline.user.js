4c8b092abd075ca13d461f1365f55ac1628de0c9
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-02-21 11:08:26 +0800

    fix: the verge underline

--- the-verge-underline.user.js
+++ the-verge-underline.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         The Verge Underline
 // @namespace    https://github.com/Vinfall/UserScripts
-// @version      1.0.0
+// @version      1.0.2
 // @author       Vinfall
 // @match        https://www.theverge.com/*
 // @grant        none
 // @description  Respect accessibility by adding back underline in hyperlinks
 // ==/UserScript==
-(function () {
-    'use strict';
-    const links = document.querySelectorAll('.clearfix a[href]');
-    links.forEach(link => {
+
+(() => {
+    const links = document.querySelectorAll('a[href]');
+    for (const link of links) {
         link.style.textDecoration = 'underline';
-    });
+    }
 })();
