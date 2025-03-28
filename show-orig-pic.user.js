ad20812887d3f54b71dea48cffffb373c8326efe
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-03-28 19:35:19 +0800

    feat: 3dmgame img

--- show-orig-pic.user.js
+++ show-orig-pic.user.js
@@ -2,7 +2,7 @@
 // @name              Show Original Picture
 // @name:zh-cn        自动跳转原图
 // @namespace         https://github.com/Vinfall/UserScripts
-// @version           0.14.4
+// @version           0.15.2
 // @author            Vinfall
 // @match             https://*.hdslb.com/bfs/*/*.avif
 // @match             https://*.hdslb.com/bfs/*/*.webp
@@ -13,6 +13,7 @@
 // @match             https://cdnfile.sspai.com/*/*/*/*.*?imageView2/2/*/interlace/*
 // @match             https://comment-img.smzdm.com/*/*/*.jpg
 // @match             https://image.gcores.com/*?x-oss-process=*
+// @match             https://img.3dmgame.com/uploads/images/*/*/*_r.*
 // @match             https://img.alicdn.com/imgextra/*/*/*_.webp
 // @match             https://img.chuapp.com//wp-content/Picture/*/*?imageView*
 // @match             https://img.chuapp.com/wp-content/Picture/*/*?imageView*
@@ -48,6 +49,7 @@
         'cdnfile.sspai.com': (url) =>
             url.replace(/(\.(png|jpg))\?imageView2\/\d+\/[^ ]*/, '$1?imageView2/2/format/webp'),
         'comment-img.smzdm.com': (url) => url.replace(/(_b\d+)?\.jpg(\.jpg)?$/, '.jpg'),
+        'img.3dmgame.com': (url) => url.replace(/(.*)_([^_]+)_r\..*$/, '$1.$2'), // 114514_jpg_r.webp -> 114514.jpg
         'img.alicdn.com': (url) => url.replace(/_\.webp$/, ''),
         'zhimg.com': (url) => url.replace(/_\d+w/, ''),
     };
