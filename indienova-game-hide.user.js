9d420d4be96a5c6683c6c77cf760699e8589d5b6
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-11-27 10:19:54 +0800

    chore/indienova: update block word
    lofi should not be low quality

--- indienova-game-hide.user.js
+++ indienova-game-hide.user.js
@@ -2,7 +2,7 @@
 // @name        indienova game hide
 // @namespace   https://github.com/Vinfall/UserScripts
 // @author      Vinfall
-// @version     5.2.12
+// @version     5.2.13
 // @match       https://indienova.com/indie-game-news/guide*
 // @match       https://indienova.com/indie-game-news/itch-new-games-*
 // @match       https://indienova.com/indie-game-news/wholesome-direct-*
@@ -23,11 +23,11 @@ TODO:
     // biome-ignore format: do not touch my keywords
     const defaultKeywords = [
         "多人在线", "恐怖", "惊悚", "僵尸", "黑暗奇幻", "自走棋", "调酒", "SCP", "种植", "农场模拟",
-        "乙女", "全动态影像", "城市营造", "社交聚会", "洛夫克拉夫特式", "打字", "自动化", "团队导向", "摔角",
+        "乙女", "全动态影像", "城市营造", "社交聚会", "洛夫克拉夫特式", "打字", "团队导向", "摔角",
         "中世纪", "刷宝", "类魂系列", "农场管理", "农业", "步行模拟", "基地建设", "AI 生成内容",
         "开放世界生存", "迷幻", "大逃杀", "4X", "玩家对战", "战锤 40K", "邪典", "益智问答", "社交推理",
-        "动作类 Rogue", "电脑角色扮演", "蓄意操控困难", "程序生成", "赌博", "集换式卡牌"
-        // "在线合作", "类银河战士恶魔城"
+        "动作类 Rogue", "电脑角色扮演", "蓄意操控困难", "程序生成", "赌博", "集换式卡牌", "放松"
+        // "在线合作", "类银河战士恶魔城", "自动化"
     ];
     const overrideDefault = true;
 
